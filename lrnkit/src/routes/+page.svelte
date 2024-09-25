41e0f141f8b871e3023e67b0e0dd2eb4c8709f86
ClassicMC <classicmclive@gmail.com>
2024-09-25 13:12:42 +0000

    giggity

---
+++ lrnkit/src/routes/+page.svelte
@@ -0,0 +1,44 @@
+<div class="main">
+  <h1>Weston <span class="primary bold">שניי</span></h1>
+  <img class="right" src="/pa.png" alt="Penn">
+  <p class="italic quote">
+    "If your mind isn’t clouded by unnecessary things, then this is the best season of your life." <br/>
+    - Wu Men
+  </p>
+  <br/>
+  <h2>Who am I?</h2>
+  <p>
+    A Frontend developer focused on <span class="skew">simplicity</span> in the web.<br/>
+    Creator of <span class="lbold">@snojs</span> and <span class="lbold">@guyotJs</span>
+  </p>
+  <br/>
+  <h2>What have I done?</h2>
+  <p>
+    I am a primarily a <span class="primary">JS</span> developer, however I do dabble in <span class="primary">Python</span> and <span class="primary">TI-Basic</span>. 
+  </p>
+  <p>
+    Big Projects:<br/>
+  </p>
+  <ol>
+    <li>The Guyot Project → <span class="italic">A content dense <span class="primary">Retro Emulator</span></span></li>
+    <li>SnoJS → <span class="italic">A lightweight <span class="primary">JS Framework</span></span></li>
+    <li>SnowyMD → <span class="italic">A <span class="primary">HTML Preprocessor</span> designed to be an Empowered Markdown</span></li>
+    <li>Polycarp → <span class="italic">A Bible app style collection of the <span class="primary">church father's</span> epistles WIP</span></li>
+  </ol>
+  <hr/>
+  <h2>More about me!</h2>
+  <p>
+    <span class="primary">2 Corinthians 4:17</span>: For this slight momentary affliction is preparing for us an eternal weight of glory beyond all comparison <br/>
+    <span class="primary">Svelte</span> is the best JS framework no questions asked <br/>
+    <span class="primary">Java</span> is the <span class="lbold">worst</span> programming language
+  </p>
+  <hr/>
+  <h2>What next...</h2>
+  <p>
+    Tell me what
+  </p><br/>
+  <a href="/guyot">Guyot is</a>
+  <a href="#ji">SnoJs is</a>
+  <a href="#ji">SnowyMd is</a>
+  <br/><br/>
+</div>
---
+++ lrnkit/src/routes/guyot/+page.svelte
@@ -0,0 +1,11 @@
+<div class="main">
+  <h1>Guyot<span class="primary bold">JS</span></h1>
+  <p class="quote italic">
+    A Retro Emulator for the web.
+  </p><br/>
+  <h2>What exactly can it do?</h2>
+  <p>
+    It can run <span class="italic">GBA, GB, N64, SNES, NES, and VBOY</span><br/>
+    
+  </p>
+</div>
