244fe1a8ec7bbf91291a4f3c41468d0fb75858c2
ClassicMC <classicmclive@gmail.com>
2024-12-02 02:36:53 +0000

    NEW SNO FEATURE IDEA

--- oden/oden.js
+++ oden/oden.js
@@ -2,36 +2,64 @@ const Oden = {
 	createListener(func){
   	let vs = func();
   },
-  $(dat,val){
-  	this.observe.push([dat, val]);
+  $(Name,Value){
+  	this.observe.push([Name, Value]);
+    this.reListen()
   },
   observe:[],
-  $get(dat){
+  $get(Variable){
   	for(let i=0;i<this.observe.length;i++){
-    	if(dat == this.observe[i][0]){
+    	if(Variable == this.observe[i][0]){
       	return this.observe[i][1]
       }
     }
+    this.reListen()
   },
-  $set(dat,change){
+  $math(Variable,Change){
   	let findVar = [];
     let newVar;
     for(let i=0;i<this.observe.length;i++){
-    	if(dat == this.observe[i][0]){
+    	if(Variable == this.observe[i][0]){
       	findVar.push(this.observe[i][1],i);
       }
     }
-    newVar = findVar[0].toString() += change;
-    this.observe[findVar[1]][1] = eval(newVar);
+    oldValue = findVar[0].toString();
+    oldValue = oldValue += Change;
+    this.observe[findVar[1]][1] = eval(oldValue);
+    this.reListen()
+  },
+  $set(Variable,Value){
+    let findVar = [];
+    for(let i=0;i<this.observe.length;i++){
+    	if(Variable == this.observe[i][0]){
+      	findVar.push(this.observe[i][1],i);
+      }
+    }
+    this.observe[findVar[1]][1] = Value;
+    this.reListen()
+  },
+  savedFunc:[],
+  createListener(Function,id){
+    this.savedFunc.push([Function,id]);
+    this.reListen()
+  },
+  reListen(){
+    for(let i=0;i<this.savedFunc.length;i++){
+      let v = this.savedFunc[i][0]()
+      document.getElementById(this.savedFunc[i][1]).innerHTML = v
+    }
+  },
+  for(Array,Template){
+    let findVar;
+    let newTemplate = "";
+    for(let i=0;i<this.observe.length;i++){
+    	if(Array == this.observe[i][0]){
+      	findVar = this.observe[i][1]
+      }
+    }
+    for(let i=0;i<findVar.length;i++){
+      newTemplate = newTemplate += Template.replaceAll(`{{${Array}}}`,findVar[i]);
+    }
+    return newTemplate;
   }
 }
-
-Oden.$("count",1)
-Oden.$set("count","+10");
-Oden.$get("count");
-
-/* function App(){
-  return [count]; 
-}
-Oden.createListener(App);
- */
